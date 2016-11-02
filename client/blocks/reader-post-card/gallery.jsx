714d19c3fe1c67afd14dc9d8574da0f0a9a23139
bluefuton <chris@bluefuton.com>
2016-11-02 14:15:05 +1300

    Convert PostGallery to stateless functional component

--- client/blocks/reader-post-card/gallery.jsx
+++ client/blocks/reader-post-card/gallery.jsx
@@ -4,36 +4,31 @@
 import React from 'react';
 import { map, take } from 'lodash';
 
-class PostGallery extends React.Component {
-
-	static propTypes = {
-		post: React.PropTypes.object.isRequired
-	}
-
-	render() {
-		const post = this.props.post;
-		const numberOfImagesToDisplay = 4;
-		const imagesToDisplay = take( post.content_images, numberOfImagesToDisplay );
-		const listItems = map( imagesToDisplay, ( image, index ) => {
-			const imageStyle = {
-				backgroundImage: 'url(' + image.src + ')',
-				backgroundSize: 'cover',
-				backgroundPosition: '50% 50%',
-				backgroundRepeat: 'no-repeat'
-			};
-			return (
-				<li key={ `post-${ post.ID }-image-${ index }` } className="reader-post-card__gallery-item">
-					<div className="reader-post-card__gallery-image" style={ imageStyle }></div>
-				</li>
-			);
-		} );
+const PostGallery = ( { post } ) => {
+	const numberOfImagesToDisplay = 4;
+	const imagesToDisplay = take( post.content_images, numberOfImagesToDisplay );
+	const listItems = map( imagesToDisplay, ( image, index ) => {
+		const imageStyle = {
+			backgroundImage: 'url(' + image.src + ')',
+			backgroundSize: 'cover',
+			backgroundPosition: '50% 50%',
+			backgroundRepeat: 'no-repeat'
+		};
 		return (
-			<ul className="reader-post-card__gallery">
-				{ listItems }
-			</ul>
+			<li key={ `post-${ post.ID }-image-${ index }` } className="reader-post-card__gallery-item">
+				<div className="reader-post-card__gallery-image" style={ imageStyle }></div>
+			</li>
 		);
-	}
+	} );
+	return (
+		<ul className="reader-post-card__gallery">
+			{ listItems }
+		</ul>
+	);
+};
 
-}
+PostGallery.propTypes = {
+	post: React.PropTypes.object.isRequired
+};
 
 export default PostGallery;
